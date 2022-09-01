0f2a61e4030cff3f2977a19e477e943b610cfb3e
Nick Kastellanos <nkastellanos@gmail.com>
2022-09-01 18:48:43 +0300

    delete prev package from localPackages

--- src/pack.bat
+++ src/pack.bat
@@ -1,3 +1,8 @@
 dotnet pack LibOVR\LibOVR.csproj /p:Configuration=Release -o LibOVR\bin\Release
 
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.libovr 1.0.0 -Source "C:\Users\usename\.nuget\localPackages" -NonInteractive
+
 "C:\Program Files (x86)\nuget\nuget.exe" add LibOVR\bin\Release\nkast.LibOVR.1.0.0.nupkg -Source "C:\Users\usename\.nuget\localPackages"
+
+
+pause
