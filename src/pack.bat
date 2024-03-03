c33a45db258089afffedf9d10417eab44663d3ec
Nick Kastellanos <nkastellanos@gmail.com>
2024-03-03 10:22:16 +0200

    ver2.0.0

--- src/pack.bat
+++ src/pack.bat
@@ -2,9 +2,9 @@ set username=username
 
 dotnet pack LibOVR\LibOVR.csproj /p:Configuration=Release -o LibOVR\bin\Release
 
-"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.libovr 1.0.0 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
+"C:\Program Files (x86)\nuget\nuget.exe" delete nkast.libovr 2.0.0 -Source "C:\Users\%username%\.nuget\localPackages" -NonInteractive
 
-"C:\Program Files (x86)\nuget\nuget.exe" add LibOVR\bin\Release\nkast.LibOVR.1.0.0.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
+"C:\Program Files (x86)\nuget\nuget.exe" add LibOVR\bin\Release\nkast.LibOVR.2.0.0.nupkg -Source "C:\Users\%username%\.nuget\localPackages"
 
 
 pause
